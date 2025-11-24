d57b27cc033611e135c0fd72c72303253e242db0
Alejandra <aleja19systems2@gmail.com>
2025-11-23 23:58:42 -0500

    Ejercicio 3 del documento entrenamiento con condicionales

---
+++ python/reto_condicional_3.py
@@ -0,0 +1,10 @@
+num = int (input ("Ingrese un número POSITIVO o NEGATIVO"))
+if num % 2 == 0:
+    print("El número es: ",num, " Es PAR ")
+else:
+    print("El número es: ",num, " Es IMPAR ")
+if num >= 0:
+    print("El número es: ",num, " Es POSITIVO")
+else:
+    print("El número es: ",num, " Es NEGATIVO")
+
